2de7ed2a7dc2300b6f3c202a05be5a7bf259fbef
Jhonny21-coder <shanemarahay@gmail.com>
2024-12-05 22:42:40 +0800

    optimized the project

--- target/classes/META-INF/VAADIN/webapp/VAADIN/build/path-utils-Duu_04h-.js
+++
@@ -1,5 +0,0 @@
-/**
- * @license
- * Copyright (c) 2023 Vaadin Ltd.
- * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
- */function u(n,t){return n.split(".").reduce((e,r)=>e?e[r]:void 0,t)}export{u as g};
--- target/classes/META-INF/VAADIN/webapp/VAADIN/static/themes/flowcrmtutorial/window.js
+++
@@ -1,4 +0,0 @@
-window.autoresize = function(textAreaElement) {
-    textAreaElement.style.height = 'auto';
-    textAreaElement.style.height = (textAreaElement.scrollHeight) + 'px';
-};
