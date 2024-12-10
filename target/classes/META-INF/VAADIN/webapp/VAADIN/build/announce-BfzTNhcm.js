2fe9d07e4c3c1a884ef8dab1d51352b9b8845ad2
Jhonny21-coder <shanemarahay@gmail.com>
2024-12-10 23:21:38 +0800

    optimized the project

--- target/classes/META-INF/VAADIN/webapp/VAADIN/build/announce-BfzTNhcm.js
+++
@@ -1,5 +0,0 @@
-import{b as a,c as l}from"./chunk-422c3a2ad42cb7c26cccff5ea772b37973c9526da5f9baabc6ba2b82c6284122-BWrh5vjC.js";/**
- * @license
- * Copyright (c) 2022 - 2023 Vaadin Ltd.
- * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
- */const e=document.createElement("div");e.style.position="fixed";e.style.clip="rect(0px, 0px, 0px, 0px)";e.setAttribute("aria-live","polite");document.body.appendChild(e);let t;function c(r,i={}){const o=i.mode||"polite",n=i.timeout===void 0?150:i.timeout;o==="alert"?(e.removeAttribute("aria-live"),e.removeAttribute("role"),t=a.debounce(t,l,()=>{e.setAttribute("role","alert")})):(t&&t.cancel(),e.removeAttribute("role"),e.setAttribute("aria-live",o)),e.textContent="",setTimeout(()=>{e.textContent=r},n)}export{c as a};
--- target/classes/META-INF/VAADIN/webapp/VAADIN/build/chunk-8a0e4cc42d2c36ebf20bdd271850d0ca51ce25c2fad28fc8e4eb50cdacf684a6-Dbpryx62.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-422c3a2ad42cb7c26cccff5ea772b37973c9526da5f9baabc6ba2b82c6284122-BWrh5vjC.js";import"./vaadin-checkbox-CNDB4VA5.js";import{i as o}from"./indexhtml-DPuqjys-.js";import{$ as m}from"./vaadin-checkbox-DCAUYS3c.js";import"./checked-mixin-Bj1S15BP.js";o(m.toString(),"CSSImport end",document);
